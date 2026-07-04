d4161000e3428d4c34d25b46361511641e3d36cb
Aryan <aryanghugare75@gmail.com>
2026-07-04 20:10:06 +0530

    Initial commit

---
+++ src/test/java/com/enterprise/EnterpriseAPP/EnterpriseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.EnterpriseAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
