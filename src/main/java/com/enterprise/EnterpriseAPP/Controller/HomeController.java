7cbd45758db98af8a9aad84cec020dc34d38a16d
Aryan <aryanghugare75@gmail.com>
2026-07-07 23:07:03 +0530

    Trial controller

---
+++ src/main/java/com/enterprise/EnterpriseAPP/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.enterprise.EnterpriseAPP.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String sayHello() {
+  return "hi , This is Aniket" ;
+
+
+    }
+
+}
